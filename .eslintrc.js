1f7bc03f6937847a8a343a703fc7e55933524905
Galileo01 <1354541676@qq.com>
2022-03-03 15:49:15 +0800

    666

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,14 @@ module.exports = {
     es2021: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+  // parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['react', 'react-hooks'],
   rules: {
